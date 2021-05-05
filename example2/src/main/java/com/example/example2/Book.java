50ed6f1876e81fa2736581f6ae3cb63014f41033
Petusin <petusin1970@gmail.com>
2021-05-05 20:06:22 +0200

    commint 2 with ID

--- example2/src/main/java/com/example/example2/Book.java
+++ example2/src/main/java/com/example/example2/Book.java
@@ -9,6 +9,9 @@ import javax.persistence.Table;
 public class Book {
 	
 	@Id
+	private int id;
+
+
 	private String title;
 	private String ISBN;
 	private int pages;
@@ -17,13 +20,22 @@ public class Book {
 	public Book() {
 		super();
 	}
+	
 	public Book(String title, String ISBN, int pages) {
 		super();
 		this.title = title;
 		this.ISBN = ISBN;
 		this.pages = pages;
 	}
+	
+	public int getId() {
+		return id;
+	}
 
+	public void setId(int id) {
+		this.id = id;
+	}
+	
 	public Book(String title) {
 		super();
 		this.title = title;
@@ -32,21 +44,15 @@ public class Book {
 		return title;
 	}
 
-	public
-
-			void setTitle(String Title) {
+	public void setTitle(String Title) {
 		this.title = Title;
 	}
 
-	public
-
-			String getISBN() {
+	public String getISBN() {
 		return ISBN;
 	}
 
-	public
-
-			void setISBN(String ISBN) {
+	public void setISBN(String ISBN) {
 		this.ISBN = ISBN;
 	}
 
@@ -57,7 +63,5 @@ public class Book {
 	public void setPages(int pages) {
 		this.pages = pages;
 	}
-
-
 
 }
